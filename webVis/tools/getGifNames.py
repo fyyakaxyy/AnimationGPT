71af69de858976d1f80fdb42c2669aec69d70e43
fyyakaxyy <fuyy28@gmail.com>
2023-11-03 17:06:17 +0800

    20231103-webVis-originalGif

---
+++ webVis/tools/getGifNames.py
@@ -0,0 +1,13 @@
+import os
+
+folder_path = 'webVis\gif\original_animation_gif'
+
+gif_files = [f for f in os.listdir(folder_path) if f.endswith('.gif')]
+
+if not gif_files:
+    print("no GIF")
+else:
+    with open('gifList.txt', 'w') as txt_file:
+        for gif_file in gif_files:
+            img_src = f'<img src="original_animation_gif\\{gif_file}">\n'
+            txt_file.write(img_src)
