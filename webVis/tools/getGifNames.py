f0691bbb900419c24183ddeaaa9a2c2a5a7afbb2
fyyakaxyy <fuyy28@gmail.com>
2023-11-04 13:38:40 +0800

    20231104-webVis and eval of Finetune-H+S3

--- webVis/tools/getGifNames.py
+++ webVis/tools/getGifNames.py
@@ -9,5 +9,5 @@ if not gif_files:
 else:
     with open('gifList.txt', 'w') as txt_file:
         for gif_file in gif_files:
-            img_src = f'<img src="webVis\gif\soul_v3_animation_gif\\{gif_file}">\n'
+            img_src = f'<img src="webVis\gif\HSmerge_animation_gif\\{gif_file}">\n'
             txt_file.write(img_src)
