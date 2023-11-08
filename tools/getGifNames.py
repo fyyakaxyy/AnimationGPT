eec51e07840b7813c8ba388112286d5223b9bf3c
fyyakaxyy <fuyy28@gmail.com>
2023-11-09 00:35:33 +0800

    20231109-smpl

--- tools/getGifNames.py
+++ tools/getGifNames.py
@@ -1,6 +1,6 @@
 import os
 
-folder_path = 'webVis\static\soul_v3'
+folder_path = 'webVis\static\original_animation_SMPL'
 
 gif_files = [f for f in os.listdir(folder_path) if f.endswith('.gif')]
 
@@ -9,5 +9,5 @@ if not gif_files:
 else:
     with open('gifList.txt', 'w') as txt_file:
         for gif_file in gif_files:
-            img_src = f'<img src="webVis\static\soul_v3\\{gif_file}">\n'
+            img_src = f'<img src="webVis\static\original_animation_SMPL\\{gif_file}">\n'
             txt_file.write(img_src)
