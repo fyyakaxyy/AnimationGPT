b2dd8f9df93a8517ed073c25076ede09dc40da52
fyyakaxyy <fuyy28@gmail.com>
2023-11-12 22:52:43 +0800

    20231112-vid2gif modify

--- tools/vid2gif.py
+++ tools/vid2gif.py
@@ -1,28 +1,39 @@
-"""
-把mp4文件转换为gif文件
-"""
+'''
+python your_script.py input_folder output_folder --scale 0.8
+'''
 
+import argparse
 from moviepy.editor import VideoFileClip
 import os
 
-input_folder = 'mp4'
-output_folder = 'gif'
+def convert_mp4_to_gif(input_folder, output_folder, scale):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+    mp4_files = [f for f in os.listdir(input_folder) if f.endswith('.mp4')]
 
-output_width = 256
-output_height = 256
+    for mp4_file in mp4_files:
+        mp4_path = os.path.join(input_folder, mp4_file)
+        gif_file = os.path.splitext(mp4_file)[0] + '.gif'
+        gif_path = os.path.join(output_folder, gif_file)
 
-mp4_files = [f for f in os.listdir(input_folder) if f.endswith('.mp4')]
+        # 使用VideoFileClip加载MP4文件
+        video_clip = VideoFileClip(mp4_path)
 
-for mp4_file in mp4_files:
-    mp4_path = os.path.join(input_folder, mp4_file)
-    gif_file = os.path.splitext(mp4_file)[0] + '.gif'
-    gif_path = os.path.join(output_folder, gif_file)
+        # 根据比例调整宽度和高度
+        output_width = int(video_clip.size[0] * scale)
+        output_height = int(video_clip.size[1] * scale)
 
-    # 使用VideoFileClip加载MP4文件
-    video_clip = VideoFileClip(mp4_path)
-    video_clip.resize((output_width, output_height)).write_gif(gif_path)
+        video_clip.resize((output_width, output_height)).write_gif(gif_path)
 
-print("--------------------------------------------Finish--------------------------------------------")
+    print("--------------------------------------------Finish--------------------------------------------")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert MP4 files to GIF files with adjustable output size.')
+    parser.add_argument('input_folder', nargs='?', default="mp4", help='Input folder containing MP4 files')
+    parser.add_argument('output_folder', nargs='?', default="gif", help='Output folder for saving GIF files')
+    parser.add_argument('--scale', type=float, default=0.3, help='Scale factor for adjusting output size (default: 0.3)')
+
+    args = parser.parse_args()
+    
+    convert_mp4_to_gif(args.input_folder, args.output_folder, args.scale)
