ec6c825ce49f5721ce32357b83c33cc3b1e68ed0
fyyakaxyy <fuyy28@gmail.com>
2024-01-08 19:33:28 +0800

    S4-3text Done

--- tools/getGifNames.py
+++ tools/getGifNames.py
@@ -1,6 +1,6 @@
 import os
 
-folder_path = 'webVis\static\S4-详版'
+folder_path = 'webVis\static\original_animation_SMPL'
 
 gif_files = [f for f in os.listdir(folder_path) if f.endswith('.gif')]
 
@@ -9,5 +9,5 @@ if not gif_files:
 else:
     with open('gifList.txt', 'w', encoding='utf-8') as txt_file:
         for gif_file in gif_files:
-            img_src = f'<img src="webVis\static\S4-详版\\{gif_file}">\n'
+            img_src = f'<img src="webVis\static\original_animation_SMPL\\{gif_file}">\n'
             txt_file.write(img_src)
