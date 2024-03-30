d1c135e26822602d532bc2fd788f7c612aed7368
fyyakaxyy <fuyy28@gmail.com>
2024-03-30 23:24:36 +0800

    20240330_vid2gif.py

--- tools/vid2gif.py
+++ tools/vid2gif.py
@@ -9,7 +9,7 @@ import argparse
 from moviepy.editor import VideoFileClip
 import os
 
-def mp4_to_gif(mp4_folder, gif_folder, scale):
+def mp4_to_gif(mp4_folder, gif_folder, scale, fps=5, colors=256):
     if not os.path.exists(gif_folder):
         os.makedirs(gif_folder)
 
@@ -23,11 +23,11 @@ def mp4_to_gif(mp4_folder, gif_folder, scale):
         # 使用VideoFileClip加载MP4文件
         video_clip = VideoFileClip(mp4_path)
 
-        # 根据比例调整宽度和高度
         gif_width = int(video_clip.size[0] * scale)
         gif_height = int(video_clip.size[1] * scale)
 
-        video_clip.resize((gif_width, gif_height)).write_gif(gif_path)
+        # 降低帧率并减少色彩位数
+        video_clip.resize((gif_width, gif_height)).write_gif(gif_path, fps=fps, colors=colors)
 
     print("--------------------------------------------Finish--------------------------------------------")
 
@@ -36,7 +36,9 @@ if __name__ == "__main__":
     parser.add_argument('mp4_folder', nargs='?', default="mp4", help='Folder of MP4 files')
     parser.add_argument('gif_folder', nargs='?', default="gif", help='Folder for saving GIF files')
     parser.add_argument('--scale', type=float, default=0.3, help='Scale factor for adjusting GIF size (default: 0.3)')
+    parser.add_argument('--fps', type=int, default=15, help='Frames per second for GIF (default: 5)')
+    parser.add_argument('--colors', type=int, default=256, help='Number of colors in GIF (default: 256)')
 
     args = parser.parse_args()
     
-    mp4_to_gif(args.mp4_folder, args.gif_folder, args.scale)
+    mp4_to_gif(args.mp4_folder, args.gif_folder, args.scale, args.fps, args.colors)
