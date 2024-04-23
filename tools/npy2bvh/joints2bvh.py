e3d453ea2361b3f55ec7c825880e4e5c5eafa82a
fyyakaxyy <fuyy28@gmail.com>
2024-04-23 13:08:55 +0800

    20240423_Tutorial

---
+++ tools/npy2bvh/joints2bvh.py
@@ -0,0 +1,177 @@
+"""将npy文件夹下的npy格式动画转为bvh格式，改编自Momask"""
+
+import visualization.Animation as Animation
+from visualization.InverseKinematics import BasicInverseKinematics, BasicJacobianIK, InverseKinematics
+from visualization.Quaternions import Quaternions
+import visualization.BVH_mod as BVH
+from visualization.remove_fs import *
+from common.skeleton import Skeleton
+import torch
+from visualization.utils.quat import ik_rot, between, fk, ik
+from tqdm import tqdm
+import numpy as np
+def plot_3d_motion(save_path, kinematic_tree, joints, title, figsize=(10, 10), fps=120, radius=4):
+    matplotlib.use('Agg')
+
+    title_sp = title.split(' ')
+    if len(title_sp) > 20:
+        title = '\n'.join([' '.join(title_sp[:10]), ' '.join(title_sp[10:20]), ' '.join(title_sp[20:])])
+    elif len(title_sp) > 10:
+        title = '\n'.join([' '.join(title_sp[:10]), ' '.join(title_sp[10:])])
+
+    def init():
+        ax.set_xlim3d([-radius / 2, radius / 2])
+        ax.set_ylim3d([0, radius])
+        ax.set_zlim3d([0, radius])
+        # print(title)
+        fig.suptitle(title, fontsize=20)
+        ax.grid(b=False)
+
+    def plot_xzPlane(minx, maxx, miny, minz, maxz):
+        ## Plot a plane XZ
+        verts = [
+            [minx, miny, minz],
+            [minx, miny, maxz],
+            [maxx, miny, maxz],
+            [maxx, miny, minz]
+        ]
+        xz_plane = Poly3DCollection([verts])
+        xz_plane.set_facecolor((0.5, 0.5, 0.5, 0.5))
+        ax.add_collection3d(xz_plane)
+
+    #         return ax
+
+    # (seq_len, joints_num, 3)
+    data = joints.copy().reshape(len(joints), -1, 3)
+    fig = plt.figure(figsize=figsize)
+    ax = p3.Axes3D(fig)
+    init()
+    MINS = data.min(axis=0).min(axis=0)
+    MAXS = data.max(axis=0).max(axis=0)
+    colors = ['red', 'blue', 'black', 'red', 'blue',
+              'darkblue', 'darkblue', 'darkblue', 'darkblue', 'darkblue',
+              'darkred', 'darkred', 'darkred', 'darkred', 'darkred']
+    frame_number = data.shape[0]
+    #     print(data.shape)
+
+    height_offset = MINS[1]
+    data[:, :, 1] -= height_offset
+    trajec = data[:, 0, [0, 2]]
+
+    data[..., 0] -= data[:, 0:1, 0]
+    data[..., 2] -= data[:, 0:1, 2]
+
+def get_grot(glb, parent, offset):
+    root_quat = np.array([[1.0, 0.0, 0.0, 0.0]]).repeat(glb.shape[0], axis=0)[:, None]
+    local_pos = glb[:, 1:] - glb[:, parent[1:]]
+    norm_offset = offset[1:] / np.linalg.norm(offset[1:], axis=-1, keepdims=True)
+    norm_lpos = local_pos / np.linalg.norm(local_pos, axis=-1, keepdims=True)
+    grot = between(norm_offset, norm_lpos)
+    grot = np.concatenate((root_quat, grot), axis=1)
+    grot /= np.linalg.norm(grot, axis=-1, keepdims=True)
+    return grot
+
+
+class Joint2BVHConvertor:
+    def __init__(self):
+        self.template = BVH.load('./visualization/data/template.bvh', need_quater=True)
+        self.re_order = [0, 1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12, 15, 13, 16, 18, 20, 14, 17, 19, 21]
+
+        self.re_order_inv = [0, 1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12, 14, 18, 13, 15, 19, 16, 20, 17, 21]
+        self.end_points = [4, 8, 13, 17, 21]
+
+        self.template_offset = self.template.offsets.copy()
+        self.parents = [-1, 0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 12, 11, 14, 15, 16, 11, 18, 19, 20]
+
+    def convert(self, positions, filename, iterations=10, foot_ik=True):
+        '''
+        Convert the SMPL joint positions to Mocap BVH
+        :param positions: (N, 22, 3)
+        :param filename: Save path for resulting BVH
+        :param iterations: iterations for optimizing rotations, 10 is usually enough
+        :param foot_ik: whether to enfore foot inverse kinematics, removing foot slide issue.
+        :return:
+        '''
+        positions = positions[:, self.re_order]
+        new_anim = self.template.copy()
+        new_anim.rotations = Quaternions.id(positions.shape[:-1])
+        new_anim.positions = new_anim.positions[0:1].repeat(positions.shape[0], axis=-0)
+        new_anim.positions[:, 0] = positions[:, 0]
+
+        if foot_ik:
+            positions = remove_fs(positions, None, fid_l=(3, 4), fid_r=(7, 8), interp_length=5,
+                                  force_on_floor=True)
+        ik_solver = BasicInverseKinematics(new_anim, positions, iterations=iterations, silent=True)
+        new_anim = ik_solver()
+
+        # BVH.save(filename, new_anim, names=new_anim.names, frametime=1 / 20, order='zyx', quater=True)
+        glb = Animation.positions_global(new_anim)[:, self.re_order_inv]
+        if filename is not None:
+            BVH.save(filename, new_anim, names=new_anim.names, frametime=1 / 20, order='zyx', quater=True)
+        return new_anim, glb
+
+    def convert_sgd(self, positions, filename, iterations=100, foot_ik=True):
+        '''
+        Convert the SMPL joint positions to Mocap BVH
+
+        :param positions: (N, 22, 3)
+        :param filename: Save path for resulting BVH
+        :param iterations: iterations for optimizing rotations, 10 is usually enough
+        :param foot_ik: whether to enfore foot inverse kinematics, removing foot slide issue.
+        :return:
+        '''
+
+        ## Positional Foot locking ##
+        glb = positions[:, self.re_order]
+
+        if foot_ik:
+             glb = remove_fs(glb, None, fid_l=(3, 4), fid_r=(7, 8), interp_length=2,
+                                 force_on_floor=True)
+
+        ## Fit BVH ##
+        new_anim = self.template.copy()
+        new_anim.rotations = Quaternions.id(glb.shape[:-1])
+        new_anim.positions = new_anim.positions[0:1].repeat(glb.shape[0], axis=-0)
+        new_anim.positions[:, 0] = glb[:, 0]
+        anim = new_anim.copy()
+
+        rot = torch.tensor(anim.rotations.qs, dtype=torch.float)
+        pos = torch.tensor(anim.positions[:, 0, :], dtype=torch.float)
+        offset = torch.tensor(anim.offsets, dtype=torch.float)
+
+        glb = torch.tensor(glb, dtype=torch.float)
+        ik_solver = InverseKinematics(rot, pos, offset, anim.parents, glb)
+        print('Fixing foot contact using IK...')
+        for i in tqdm(range(iterations)):
+            mse = ik_solver.step()
+            # print(i, mse)
+
+        rotations = ik_solver.rotations.detach().cpu()
+        norm = torch.norm(rotations, dim=-1, keepdim=True)
+        rotations /= norm
+
+        anim.rotations = Quaternions(rotations.numpy())
+        anim.rotations[:, self.end_points] = Quaternions.id((anim.rotations.shape[0], len(self.end_points)))
+        anim.positions[:, 0, :] = ik_solver.position.detach().cpu().numpy()
+        if filename is not None:
+            BVH.save(filename, anim, names=new_anim.names, frametime=1 / 20, order='zyx', quater=True)
+        # BVH.save(filename[:-3] + 'bvh', anim, names=new_anim.names, frametime=1 / 20, order='zyx', quater=True)
+        glb = Animation.positions_global(anim)[:, self.re_order_inv]
+        return anim, glb
+
+
+
+if __name__ == "__main__":
+    # 需要转换的npy文件
+    folder = 'npy_folder'
+    files = os.listdir(os.path.join(folder))
+    # files = [f for f in files if 'repeat' in f]
+
+    # 保存转换后bvh文件的路径
+    if not os.path.exists('bvh_folder'):
+            os.makedirs('bvh_folder')
+    converter = Joint2BVHConvertor()
+    for f in tqdm(files):
+        joints = np.load(os.path.join(folder, f))
+        joints = np.squeeze(joints, axis=0) # mgpt生成的motion.npy和Momask的npy维度不一样，在此处修改
+        converter.convert(joints, os.path.join('bvh_folder', f'bvh_{f}'.replace('npy', 'bvh')), foot_ik=False)
